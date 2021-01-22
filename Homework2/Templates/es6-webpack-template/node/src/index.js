f0f60150645804ec22ffbd2eba1e7314a3e75641
Seongwoo Choi <swchoi1994@gmail.com>
2021-01-22 23:13:28 +0900

    The second commit to the Homework2

--- Homework2/Templates/es6-webpack-template/node/src/index.js
+++ Homework2/Templates/es6-webpack-template/node/src/index.js
@@ -1,15 +1,13 @@
-
+// This is the end of trial!!! 
 import testData from "./data/test.json";
-//import filmData from "./datasets/sf_movies.csv"; /* Example of reading in data */
+ /* Example of reading in data */
 import './css/style.scss';
 import './css/styles.css'; /* Example of connecting a style-sheet */
 import {drawBarChart} from "./js/barchart"; /* Example of importing one function from a js file for multiple {f(x), f(y), f(z)}*/
-import {drawMap} from "./js/map";
 
 let x = 2;
 console.log(testData);
 drawBarChart(testData["data"], "#bar");
-drawMap(testData["data"],"#map");
 
 
 /* 
